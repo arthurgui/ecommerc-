1a60bfe21c582b7a5da2f712ea25e1aa19fe3857
unknown <arthurpereira.g@gmail.com>
2021-02-23 22:43:30 -0300

    Criando o Page admin e o tpl admin

---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Hcode;
+
+class PageAdmin extends Page{
+
+    public function __construct($opts = array(), $tpl_dir = "/views/admin/"){
+
+        //passando como herança
+        parent::__construct($opts, $tpl_dir);
+
+    }
+
+}
+
+?>
